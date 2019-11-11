e5ae984d6c24e7fd64557428ac0f0efdf75e586c
zbl <65335673@qq.com>
2019-11-11 18:51:47 +0800

    11-11

--- pages/sendTicketConfigChild/sendTicketConfigChild.js
+++ pages/sendTicketConfigChild/sendTicketConfigChild.js
@@ -1,66 +1,198 @@
 // pages/sendTicketConfigChild/sendTicketConfigChild.js
+var utils = require("../../utils/util.js")
+const app = getApp();
 Page({
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        checked: true,
+        checked1: true,
+        radio: '1',
+        list: ['a', 'b', 'c'],
+        result: ['a', 'b'],
+        itemList: [
+            { id: 1, name: 'C语言', isSelected: false, },
+            { id: 2, name: 'Java', isSelected: false, },
+            { id: 3, name: 'C++', isSelected: false, },
+        ],
+        memberList: [{
+                id: 1,
+                name: 'B语言',
+                isSelected: true,
+                invalidActivty: [{
+                    cdName: "哈LOL",
+                    cdId: 1,
+                    cdType: true,
+                }, {
+                    cdName: "bbbb",
+                    cdId: 2,
+                    cdType: true,
+                }, {
+                    cdName: "ccc",
+                    cdId: 3,
+                    cdType: true,
+                }]
+            },
+            {
+                id: 1,
+                name: 'Java',
+                isSelected: true,
+                invalidActivty: [{
+                    cdName: "eee",
+                    cdId: 1,
+                    cdType: true,
+                }, {
+                    cdName: "ffff",
+                    cdId: 2,
+                    cdType: true,
+                }, {
+                    cdName: "gggg",
+                    cdId: 3,
+                    cdType: true,
+                }]
+            }
+        ]
+    },
 
-  },
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function(options) {
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
+    },
+    // onChange({ detail,e }) {
+    //     console.log(detail,e)
+    //         // 需要手动对 checked 状态进行更新
+    //     this.setData({ checked: detail });
+    // },
+    onChange(e) {
+        console.log(e);
+        let that = this;
+        let detail = e.detail;
+        let index = e.currentTarget.dataset.index;
+        let checked1 = that.data.checked1;
+        if (index == 1) {
+            that.setData({
+                checked: detail
+            })
+        } else {
+            // 需要手动对 checked 状态进行更新
+            that.setData({
+                checked1: detail,
+                radio: 1
+            });
+        }
 
-  },
+    },
+    radioChange(event) {
+        console.log(event)
+        this.setData({
+            radio: event.detail
+        });
+    },
+    checkChange(event) {
+        console.log(event)
+        this.setData({
+            result: event.detail
+        });
+    },
+    toggle(event) {
+        const { index } = event.currentTarget.dataset;
+        const checkbox = this.selectComponent(`.checkboxes-${index}`);
+        checkbox.toggle();
+    },
+    onClick(event) {
+        const { name } = event.currentTarget.dataset;
+        this.setData({
+            radio: name
+        });
+    },
+    noop() {},
+    itemSelected: function(e) {
+        console.log(e)
+        var index = e.currentTarget.dataset.index;
+        var item = this.data.itemList[index];
+        item.isSelected = !item.isSelected;
+        this.setData({
+            itemList: this.data.itemList,
+        });
+    },
+    isOpen(e) {
+        var that = this;
+        var idx = e.currentTarget.dataset.index;
+        console.log(idx);
+        var memberList = that.data.memberList;
+        console.log(memberList);
+        for (let i in memberList) {
+            let invalidActivty = memberList[i].invalidActivty;
+            for (let i in invalidActivty) {
+                invalidActivty[i].fName = "";
+                console.log(invalidActivty[i].cdName.substr(0, 1))
+                invalidActivty[i].fName = invalidActivty[i].cdName.substr(0, 1)
+            }
+        }
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
 
-  },
+        for (let i = 0; i < memberList.length; i++) {
+            if (idx == i) {
+                memberList[i].isSelected = !memberList[i].isSelected;
+            } else {
+                memberList[i].isSelected = true;
+            }
+        }
+        that.setData({ memberList: memberList });
+        return true;
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
+    },
 
-  },
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function() {
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
+    },
 
-  },
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function() {
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
+    },
 
-  },
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide: function() {
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
+    },
 
-  },
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload: function() {
 
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
+    },
 
-  },
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function() {
 
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
+    },
 
-  }
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function() {
+
+    },
+
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function() {
+
+    }
 })
--- utils/util.js
+++ utils/util.js
@@ -1,4 +1,5 @@
 const formatTime = date => {
+  var date = new Date(date);
   const year = date.getFullYear()
   const month = date.getMonth() + 1
   const day = date.getDate()
@@ -37,8 +38,84 @@ function consoleLog(msg) {
   console.log(msg);
 
 }
+
+/**
+* 日期格式化
+* value 将要格式化的值
+* format 格式化的格式
+**/
+function sysFormatDate(value, format, isapi) {
+  if (!value) {
+    return "";
+  }
+  if (isapi) {
+    value = GetDateRemoveT(value);
+  }
+  var date;
+  if (/\/Date\((.*)\+.*\//.test(value)) {
+    value = RegExp.$1;
+
+    date = new Date();
+    date.setTime(value);
+  } else {
+    if (typeof value === "string") {
+      date = new Date(value);
+    } else if (typeof value === "object") {
+      date = value;
+    } else {
+      return "";
+    }
+  }
+
+  try {
+    var o = {
+      "M+": date.getMonth() + 1,
+      "d+": date.getDate(),
+      "h+": date.getHours(),
+      "m+": date.getMinutes(),
+      "s+": date.getSeconds(),
+      "q+": Math.floor((date.getMonth() + 3) / 3),
+      "S": date.getMilliseconds()
+    }
+
+    if (/(y+)/.test(format)) {
+      format = format.replace(RegExp.$1, (date.getFullYear() + "").substr(4 - RegExp.$1.length));
+    }
+
+    for (var k in o) {
+      if (new RegExp("(" + k + ")").test(format)) {
+        format = format.replace(RegExp.$1, RegExp.$1.length == 1 ? o[k] : ("00" + o[k]).substr(("" + o[k]).length));
+      }
+    }
+  } catch (e) {
+    return "";
+  }
+  return format;
+}
+
+function GetDateRemoveT(d) {
+  var date = d.replace('T', ' ');
+  return date;
+}
+/**
+* 格式化金额，抹掉结尾的.00
+**/
+function syFormatMoney(value) {
+  if (!value) {
+    return "0";
+  }
+
+  if (typeof value !== "string") {
+    value = value.toString();
+  }
+
+  return value.replace(/^(\d+)(?:\.0+$|(\.\d+?)0+$)/, "$1$2");
+}
+
 module.exports = {
   formatTime: formatTime,
   AjaxRequest: AjaxRequest,
+  sysFormatDate: sysFormatDate,
+  syFormatMoney: syFormatMoney,
   Console: consoleLog
 }
