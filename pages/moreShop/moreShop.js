d0776e3a33c0a989bb67d7d4842c3b000457aa8b
zbl <65335673@qq.com>
2019-11-21 15:19:54 +0800

    11

--- pages/moreShop/moreShop.js
+++ pages/moreShop/moreShop.js
@@ -1,4 +1,7 @@
 // pages/moreShop/moreShop.js
+var utils = require("../../utils/util.js")
+const app = getApp();
+var page = 1; //初始化页数
 Page({
 
     /**
@@ -16,12 +19,82 @@ Page({
             address: "芙蓉区五一大道人瑞潇湘国际一楼213号"
         }, ]
     },
+  onBindReceiveTap: function (event) {
+    wx.navigateTo({
+      url: '../ticketMes/ticketMes?CouponID=' + event.currentTarget.dataset.couponid + '&ReleaseID=' + event.currentTarget.dataset.releaseid,
+    })
+  },
+
+  GetData: function (page) {
+    let that = this;
+    //显示 加载中的提示
+    wx.showLoading({
+      title: '数据加载中...',
+    })
+    var data = {};
+    data.pGroupID = app.globalData.AppGroupInfo.GroupID;
+    data.pPageIndex = page;
+    data.pPageSize = 1;
+    data.pLatitudeX = app.globalData.latitudeX;
+    data.pLongitudeY = app.globalData.longitudeY;
+    utils.AjaxRequest(app.globalData.apiurl + "CouponView/CoupoInfoView/QueryCouponInfo", "POST", data, app.globalData.appkeyid, this.GetDataBack)
+  },
+  GetDataBack: function (json) {
+    let that = this;
+    console.log(json);
+    var json = json.data.Data;
+    if (json.flag) {
+      console.log(json.msg);
+      if (page == 1) {
+        var banner = [];
+        var popular = [];
+        var extension = [];
+        json.data.forEach(item => {
+          console.log(JSON.stringify(item));
+          if (item.Attribute == 3) {
+            banner.push(item);
+          }
+          if (item.Attribute == 2) {
+            popular.push(item);
+          }
+          if (item.Attribute == 1) {
+            extension.push(item);
+          }
+          if (item.Attribute == 0) {
+
+          }
+
+        });
+
+        that.setData({
+          images: banner,
+          imgUrls: popular,
+          shopArrays: extension,
+          hotTicketBox: json.data,
+          lastpage: json.pageCount //你的总页数
+        });
+      } else {
+        //获取上次加载的数据
+        var oldlists = that.data.hotTicketBox;
+        var newlists = oldlists.concat(json.data) //合并数据 res.data 你的数组数据
+
+        that.setData({
+          hotTicketBox: newlists,
+          lastpage: json.pageCount //你的总页数   
+        });
+      }
+    }
+
+    //隐藏 加载中的提示
+    wx.hideLoading();
+  },
 
     /**
      * 生命周期函数--监听页面加载
      */
     onLoad: function(options) {
-
+      page = 1;
+      that.GetData(page);
     },
 
     /**
