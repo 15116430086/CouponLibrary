a5fb52d014b59bbbb67d7b2ea8d97660693654c7
zbl <65335673@qq.com>
2019-11-22 13:11:41 +0800

    1

--- pages/chooseTicket/chooseTicket.js
+++ pages/chooseTicket/chooseTicket.js
@@ -121,7 +121,7 @@ Page({
     var data = {};
     data.pGroupID = app.globalData.AppGroupInfo.GroupID;
     data.pPageIndex = page;
-    data.pPageSize = 1;
+    data.pPageSize = 5;
     data.pLatitudeX = app.globalData.latitudeX;
     data.pLongitudeY = app.globalData.longitudeY;
     utils.AjaxRequest(app.globalData.apiurl + "CouponView/CoupoInfoView/QueryCouponInfo", "POST", data, app.globalData.appkeyid, this.GetDataBack)
