b50185db7f8a607769851bb144dbb471a4316bbf
ztg <1971922130@qq.com>
2019-11-29 10:06:26 +0800

    11

--- utils/util.js
+++ utils/util.js
@@ -38,7 +38,7 @@ function AjaxRequest(pUrl, pType, pData, pAppKeyId, pCallBack) {
     },
     fail(res) {
       wx.showToast({
-        title:'服务器连接失败' //'请求失败',
+        title: '服务器连接失败' //'请求失败',
       })
     }
   })
@@ -129,9 +129,9 @@ function syJsonSafe(obj) {
 }
 
 
-function UploadImg(count,pGroupID,type, pAppKeyId, pCallBack, pOther) {
-  var url ="http://test.miboon.com/LibraryAPI/CouponView/UploadImageView/UploadImgs";
-  if(type==1){
+function UploadImg(count, pGroupID, type, pAppKeyId, pCallBack, pOther) {
+  var url = "http://test.miboon.com/LibraryAPI/CouponView/UploadImageView/UploadImgs";
+  if (type == 1) {
     url = "http://test.miboon.com/LibraryAPI/CouponView/UploadImageView/UploadBusinesslicense";
   }
   wx.chooseImage({
@@ -157,18 +157,19 @@ function UploadImg(count,pGroupID,type, pAppKeyId, pCallBack, pOther) {
           }, // HTTP 请求中其他额外的参数
           success: function(res) {
             var json = JSON.parse(res.data);
-            if (type == 1) {//说明是营业执照
+            if (type == 1) { //说明是营业执照
               imgs.push(json.Data.imgurl);
               imgs.push(json.Data.data);
-            }else{
-            json.Data = json.Data.replace("true|", "");
-            imgs.push(json.Data);
+            } else {
+              json.Data = json.Data.replace("true|", "");
+              imgs.push(json.Data);
+            }
             if (imgs.length == tempFilePaths.length) {
               wx.hideLoading();
-           
+
               pCallBack(imgs, pOther);
             }
-          }
+
           },
           fail: function(res) {}
         });
@@ -245,7 +246,7 @@ function GetRegionIndustryBack(json, pCallBack) {
   }
 }
 
-function reverseGeocoder(latitude, longitude,pCallBack) {       
+function reverseGeocoder(latitude, longitude, pCallBack) {       
   qqmapsdk.reverseGeocoder({    
     location: {
       latitude: latitude,
@@ -287,7 +288,7 @@ function reverseGeocoder(latitude, longitude,pCallBack) {       
 
 }
 
-function getGeocoder(address, region,pCallBack) {
+function getGeocoder(address, region, pCallBack) {
   //调用地址解析接口
   qqmapsdk.geocoder({
     //获取表单传入地址
