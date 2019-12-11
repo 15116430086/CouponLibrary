3b59d1e2fbb45ad4d1449a55e8adbd1253364ff0
linmxxx <348449223@qq.com>
2019-12-11 16:00:48 +0800

    111

--- pages/chooseBuyGoods/chooseBuyGoods.js
+++ pages/chooseBuyGoods/chooseBuyGoods.js
@@ -9,102 +9,125 @@ Page({
      */
     data: {
         result: [],
-      userlist: [],
-      lastpage: 0,
+        userlist: [],
+        lastpage: 0,
+        checked: true,
+        checked1: true
     },
 
     /**
      * 生命周期函数--监听页面加载
      */
     onLoad: function(options) {
-      let that = this;
-      that.GetData();
-  
+        let that = this;
+        that.GetData();
+
     },
 
 
-  GetData: function () {
-    let that = this;
-    //显示 加载中的提示
-    wx.showLoading({
-      title: '数据加载中...',
-    })
-    var data = {};
-    data.pGroupID = app.globalData.AppGroupInfo.GroupID;
-    data.pPageIndex = page;
-    data.pPageSize = 20;
-    utils.AjaxRequest(app.globalData.apiurl + "CouponView/CouponProductView/GetPageCouponProduct", "POST", data, app.globalData.appkeyid, this.GetDataBack)
-  },
-  GetDataBack: function (json) {
-    let that = this;
-    var json = json.data.Data;
-    wx.hideLoading();
-    if (json.flag) {
-      console.log(json.msg);
-      console.log(json)
-      if (page == 1) {
-        this.setData({
-          userlist: json.data,
-          lastpage: json.pageCount //你的总页数   
-        });
-      } else {
-        //获取上次加载的数据
-        var oldlists = that.data.userlist;
-        var newlists = oldlists.concat(json.data) //合并数据 res.data 你的数组数据
-        that.setData({
-          userlist: newlists,
-          lastpage: json.pageCount //你的总页数   
-        });
-      }
-      that.setData({
-        result: wx.getStorageSync("pArrProductKey")
-      });
-      
-    } else {
-      wx.showToast({
-        title: '没有找到相关数据!',
-        icon: 'none',
-        duration: 2000
-      })
-    }
-  },
+    GetData: function() {
+        let that = this;
+        //显示 加载中的提示
+        wx.showLoading({
+            title: '数据加载中...',
+        })
+        var data = {};
+        data.pGroupID = app.globalData.AppGroupInfo.GroupID;
+        data.pPageIndex = page;
+        data.pPageSize = 20;
+        utils.AjaxRequest(app.globalData.apiurl + "CouponView/CouponProductView/GetPageCouponProduct", "POST", data, app.globalData.appkeyid, this.GetDataBack)
+    },
+    GetDataBack: function(json) {
+        let that = this;
+        var json = json.data.Data;
+        wx.hideLoading();
+        if (json.flag) {
+            console.log(json.msg);
+            console.log(json)
+            if (page == 1) {
+                this.setData({
+                    userlist: json.data,
+                    lastpage: json.pageCount //你的总页数   
+                });
+            } else {
+                //获取上次加载的数据
+                var oldlists = that.data.userlist;
+                var newlists = oldlists.concat(json.data) //合并数据 res.data 你的数组数据
+                that.setData({
+                    userlist: newlists,
+                    lastpage: json.pageCount //你的总页数   
+                });
+            }
+            that.setData({
+                result: wx.getStorageSync("pArrProductKey")
+            });
+
+        } else {
+            wx.showToast({
+                title: '没有找到相关数据!',
+                icon: 'none',
+                duration: 2000
+            })
+        }
+    },
 
     onChange(event) {
-      let that = this;
- 
-      console.log(event)
+        let that = this;
+
+        console.log(event)
         this.setData({
             result: event.detail
         });
-      
+
     },
 
-  jumpBack(e){
-    let that = this;
-    var pages = getCurrentPages()
-    let thispage = pages[pages.length - 1]
-    let parPage = pages[pages.length - 2] //上级页面
-    let result = that.data.result;
-    console.log(parPage)
-    if(result.length>0){
-      parPage.setData({
-        pArrProductID: result
-      })
+    jumpBack(e) {
+        let that = this;
+        var pages = getCurrentPages()
+        let thispage = pages[pages.length - 1]
+        let parPage = pages[pages.length - 2] //上级页面
+        let result = that.data.result;
+        console.log(parPage)
+        if (result.length > 0) {
+            parPage.setData({
+                pArrProductID: result
+            })
 
-      wx.setStorageSync("pArrProductKey", result);
+            wx.setStorageSync("pArrProductKey", result);
 
-      wx.navigateBack({
-        delta: 1
-      })
-    }else{
-      wx.showToast({
-        title: '请选择关联商品!',
-        icon: 'none',
-        duration: 2000
-      })
-    }
-    
-  },
+            wx.navigateBack({
+                delta: 1
+            })
+        } else {
+            wx.showToast({
+                title: '请选择关联商品!',
+                icon: 'none',
+                duration: 2000
+            })
+        }
+
+    },
+
+
+
+    swChange(e) {
+        console.log(e);
+        let that = this;
+        let detail = e.detail;
+        let index = e.currentTarget.dataset.index;
+        let checked1 = that.data.checked1;
+        if (index == 1) {
+            that.setData({
+                checked: detail
+            })
+        } else {
+            // 需要手动对 checked 状态进行更新
+            that.setData({
+                checked1: detail
+            });
+        }
+
+    },
     /**
      * 生命周期函数--监听页面初次渲染完成
      */
@@ -140,23 +163,23 @@ Page({
 
     },
 
-  /**
-  * 页面上拉触底事件的处理函数
-  */
-  onReachBottom: function () {
-    let that = this;
-    if (that.data.lastpage > page) {
-      page++
-      that.GetData(page);
-    } else if (that.data.lastpage == page) {
-      page++;
-      wx.showToast({
-        title: '没有更多数据!',
-        icon: 'none',
-        duration: 2000
-      })
-    }
-  },
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function() {
+        let that = this;
+        if (that.data.lastpage > page) {
+            page++
+            that.GetData(page);
+        } else if (that.data.lastpage == page) {
+            page++;
+            wx.showToast({
+                title: '没有更多数据!',
+                icon: 'none',
+                duration: 2000
+            })
+        }
+    },
     /**
      * 用户点击右上角分享
      */
