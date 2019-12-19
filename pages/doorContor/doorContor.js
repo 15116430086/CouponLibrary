8e42af1f61bbc0a3f1ce7589ed912bf04c9fa8a7
ztg <1971922130@qq.com>
2019-12-19 17:38:18 +0800

    11

--- pages/doorContor/doorContor.js
+++ pages/doorContor/doorContor.js
@@ -62,7 +62,7 @@ Page({
             address: res.address
           })
 
-          utils.getGeocoder(res.address, app.globalData.regionName, chat.getGeocoderBack)
+          utils.getGeocoder(res.address,chat.getGeocoderBack)
         }
       }
     })
