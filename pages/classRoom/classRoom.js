5ef3eeb303348a43221a7c68eb0f36a561737158
linmxxx <348449223@qq.com>
2019-12-26 10:13:13 +0800

    111

--- pages/classRoom/classRoom.js
+++ pages/classRoom/classRoom.js
@@ -7,96 +7,108 @@ Page({
    * 页面的初始数据
    */
   data: {
-    pageIndex:1,
-    pageSize:10,
-    datalist:{},
-    isRefresh:true
+    pageIndex: 1,
+    pageSize: 10,
+    datalist: {},
+    isRefresh: true
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-    var data={
+  onLoad: function(options) {
+    var data = {
       pPageIndex: this.data.pageIndex,
       pPageSize: this.data.pageSize
     };
     utils.AjaxRequest(app.globalData.apiurl + "CouponView/CouponHomeView/GetQuestionBank", "POST", data, app.globalData.appkeyid, this.GetQuestionBank)
   },
-  jumpWeb(e){
+  jumpWeb(e) {
     var url = e.currentTarget.dataset.url;
-    wx.navigateTo({
-      url: '../webView/webView?url='+url,
-    })
+
+    let index = url.lastIndexOf(".");
+    let ext = url.substr(index + 1);
+    console.log(ext)
+    if (ext == "png") {
+      wx.navigateTo({
+        url: '../imageView/imageView?url=' + url,
+      })
+
+    } else {
+      wx.navigateTo({
+        url: '../webView/webView?url=' + url,
+      })
+    }
+
   },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  GetQuestionBank: function (res) {
-    var chat=this;
-    var json=res.data.Data;
-    if(json.flag){
-      if (chat.data.pageIndex==1){
+  GetQuestionBank: function(res) {
+    var chat = this;
+    var json = res.data.Data;
+    if (json.flag) {
+      if (chat.data.pageIndex == 1) {
         chat.setData({
-          pageIndex: chat.data.pageIndex+1,
+          pageIndex: chat.data.pageIndex + 1,
           datalist: json.data
         });
-      }else{
+      } else {
         var list = chat.data.datalist
         var newlists = list.concat(json.data) //合并数据 res.data 你的数组数据
         chat.setData({
           datalist: newlists,
-          pageIndex:chat.data.pageIndex+1
+          pageIndex: chat.data.pageIndex + 1
         });
       }
-    }else{
-      if (chat.data.pageIndex == 1){
+    } else {
+      if (chat.data.pageIndex == 1) {
         wx.showToast({
           title: json.msg,
           icon: "暂无数据"
         });
-      }else{
+      } else {
         chat.setData({
-          isRefresh:false
+          isRefresh: false
         });
-        
+
       }
-      
+
     }
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
     var data = {
       pPageIndex: this.data.pageIndex,
       pPageSize: this.data.pageSize
@@ -108,7 +120,7 @@ Page({
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
