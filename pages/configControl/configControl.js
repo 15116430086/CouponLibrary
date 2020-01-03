93cd1d314cb1af974cb04f84e4da226c0025b35d
zbl <65335673@qq.com>
2020-01-03 16:51:57 +0800

    z

--- pages/configControl/configControl.js
+++ pages/configControl/configControl.js
@@ -12,70 +12,80 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
 
   },
   onChange(e) {
-    console.log(e);
+    //console.log(e);
     let that = this;
     let detail = e.detail;
     let index = e.currentTarget.dataset.index;
-    if (index == 1) {
+    if (index == 0) {
       that.setData({
         checked: detail
       })
+      if (detail) {
+        wx.requestSubscribeMessage({
+          tmplIds: ['i2IVNqj5k05F-mBNOEtM7HMwdSoEJBhHKKC49r1zpkc'],
+          success: function(e) {
+
+          }
+        })
+      }
     } else {
       // 需要手动对 checked 状态进行更新
-      that.setData({ checked1: detail });
+      that.setData({
+        checked1: detail
+      });
     }
 
   },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
