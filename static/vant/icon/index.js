bd2dce37c5f974ef054ffedf3bf3a65a56330db7
linmxxx <348449223@qq.com>
2020-03-17 10:00:09 +0800

    11

--- static/vant/icon/index.js
+++ static/vant/icon/index.js
@@ -1,39 +1,19 @@
 import { VantComponent } from '../common/component';
-import { addUnit } from '../common/utils';
 VantComponent({
     props: {
-        dot: Boolean,
         info: null,
-        size: {
-            type: null,
-            observer: 'setSizeWithUnit'
-        },
+        name: String,
+        size: String,
         color: String,
         customStyle: String,
         classPrefix: {
             type: String,
             value: 'van-icon'
-        },
-        name: {
-            type: String,
-            observer(val) {
-                this.setData({
-                    isImageName: val.indexOf('/') !== -1
-                });
-            }
         }
-    },
-    data: {
-        sizeWithUnit: null,
     },
     methods: {
         onClick() {
             this.$emit('click');
-        },
-        setSizeWithUnit(size) {
-            this.setData({
-                sizeWithUnit: addUnit(size)
-            });
         }
     }
 });
