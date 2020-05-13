2f4c2075ec42551e78e357fa88b3a4f165b35019
zbl <65335673@qq.com>
2020-05-13 13:38:11 +0800

    1

--- pages/doorScan/doorScan.js
+++ pages/doorScan/doorScan.js
@@ -7,33 +7,33 @@ Page({
    * 页面的初始数据
    */
   data: {
-      img:""
+    img: ""
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-    if(options.shopid)
-    {//新增员工二维码
-    var datas={
-      ShopID: options.shopid
+  onLoad: function(options) {
+    if (options.shopid) { //新增员工二维码
+      var datas = {
+        ShopID: options.shopid
+      }
+      utils.AjaxRequest(app.globalData.apiurl + "CouponView/CouponShopView/StoreQRcode", "POST", datas, app.globalData.appkeyid, this.CouponShopView);
     }
-    utils.AjaxRequest(app.globalData.apiurl + "CouponView/CouponShopView/StoreQRcode", "POST", datas, app.globalData.appkeyid, this.CouponShopView);
-    }
-    
-    if(options.gid)
-    {//店铺分享 小程序码
+
+    if (options.gid) { //店铺分享 小程序码
       var datas = {
         gid: options.gid,
         ShopID: options.shopid
       }
       utils.AjaxRequest(app.globalData.apiurl + "CouponView/CouponGroupView/StoreQRcode", "POST", datas, app.globalData.appkeyid, this.CouponShopView);
-    }    
+    }
   },
-  CouponShopView:function(res){
-   var json=res.data.Data;
-   var chat=this;
-    chat.setData({ img:json.QRCodeImg});
+  CouponShopView: function(res) {
+    var json = res.data.Data;
+    var chat = this;
+    chat.setData({
+      img: json.QRCodeImg
+    });
   }
 })
