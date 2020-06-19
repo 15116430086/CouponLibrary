d71cf5fccb32dd45a2cbd3328099c5c6ec96ee8f
zbl <65335673@qq.com>
2020-06-19 17:08:20 +0800

    1

--- pages/shopDetail/shopDetail.js
+++ pages/shopDetail/shopDetail.js
@@ -8,9 +8,9 @@ Page({
    */
   data: {
     date: "日期",
-    hidden:false,
+    hidden: false,
     currentDate: new Date().getTime(),
-    minDate:2020,
+    minDate: new Date().getTime('2020-01-01'),
     formatter(type, value) {
       if (type === 'year') {
         return `${value}年`;
@@ -20,15 +20,15 @@ Page({
       return value;
     },
     show: false,
-    datatime:"",
-    valuetime:"",
-    types:1,
-    DataList:[],
-    DataList2:[],
-    ststics:0,
-    titleName:"请输入券名称",
-    search:"",
-    searchTime:""
+    datatime: "",
+    valuetime: "",
+    types: 1,
+    DataList: [],
+    DataList2: [],
+    ststics: 0,
+    titleName: "请输入券名称",
+    search: "",
+    searchTime: ""
   },
 
   /**
@@ -36,73 +36,84 @@ Page({
    */
   onLoad: function (options) {
     console.log(options)
-    if(options.types==2){
-      this.setData({titleName:"请输入店名称"});
+    if (options.types == 2) {
+      this.setData({
+        titleName: "请输入店名称"
+      });
     }
     this.setData({
-      datatime:options.time=='日期'?'':options.time,
-      valuetime:options.name,
-      types:options.types
+      datatime: options.time == '日期' ? '' : options.time,
+      valuetime: options.name,
+      types: options.types
     });
   },
 
-  UserQuery:function(){
+  UserQuery: function () {
     wx.showLoading({
       title: '数据加载中...',
     })
-    var data={
-      Bingtime:this.data.valuetime,
-      Time:this.data.datatime,
-      pGroupID:app.globalData.AppGroupInfo.GroupID,
-      type:this.data.ststics,
-      ShopName:this.data.search
+    var data = {
+      Bingtime: this.data.valuetime,
+      Time: this.data.datatime,
+      pGroupID: app.globalData.AppGroupInfo.GroupID,
+      type: this.data.ststics,
+      ShopName: this.data.search
     }
     utils.AjaxRequest(app.globalData.apiurl + "CouponView/withdrawalAccountView/GetUserList", "POST", data, app.globalData.appkeyid, this.GetUserList)
   },
 
-  GetUserList:function(res) {
+  GetUserList: function (res) {
     wx.hideLoading();
-    var json=res.data.Data;
-     var chat=this;
-     chat.setData({DataList:json.data,searchTime:json.datetime});
+    var json = res.data.Data;
+    var chat = this;
+    chat.setData({
+      DataList: json.data,
+      searchTime: json.datetime
+    });
   },
 
 
-  CouponQuery:function(){
+  CouponQuery: function () {
     wx.showLoading({
       title: '数据加载中...',
     })
-    var data={
-      Bingtime:this.data.valuetime,
-      Time:this.data.datatime,
-      pGroupID:app.globalData.AppGroupInfo.GroupID,
-      type:this.data.ststics,
-      CouponName:this.data.search
+    var data = {
+      Bingtime: this.data.valuetime,
+      Time: this.data.datatime,
+      pGroupID: app.globalData.AppGroupInfo.GroupID,
+      type: this.data.ststics,
+      CouponName: this.data.search
     }
     utils.AjaxRequest(app.globalData.apiurl + "CouponView/withdrawalAccountView/GetCouponList", "POST", data, app.globalData.appkeyid, this.GetCouponList)
   },
-  GetCouponList:function(res){
+  GetCouponList: function (res) {
     wx.hideLoading();
-    var json=res.data.Data;
-     var chat=this;
-     chat.setData({DataList2:json.data,searchTime:json.datetime});
-  },
-  username:function(event){
-    this.setData({
-      search:event.detail.value
+    var json = res.data.Data;
+    var chat = this;
+    chat.setData({
+      DataList2: json.data,
+      searchTime: json.datetime
     });
   },
-  seacrh:function(){
-    if(this.data.types==1){
+  username: function (event) {
+    this.setData({
+      search: event.detail.value
+    });
+  },
+  seacrh: function () {
+    if (this.data.types == 1) {
       this.UserQuery();
-    }else{
+    } else {
       this.CouponQuery();
     }
   },
-   // 显示日期
-   showTime(e){
+  // 显示日期
+  showTime(e) {
     let that = this;
-    that.setData({ show: true,hidden:true });
+    that.setData({
+      show: true,
+      hidden: true
+    });
   },
 
   // 日期选择
@@ -113,18 +124,18 @@ Page({
     timer = utils.formatTimeyears(timer);
     console.log(timer)
     that.setData({
-        date: timer,
-        show: false,
-        hidden:false,
-        ststics:1,
-        datatime:timer
+      date: timer,
+      show: false,
+      hidden: false,
+      ststics: 1,
+      datatime: timer
     })
-    if(that.data.types==1){
+    if (that.data.types == 1) {
       that.UserQuery();
-    }else{
+    } else {
       that.CouponQuery();
     }
-},
+  },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
@@ -137,14 +148,18 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-    if(this.data.types==1){
+    if (this.data.types == 1) {
       this.UserQuery();
-    }else{
+    } else {
       this.CouponQuery();
     }
-     
-  },
 
+  },
+  onClose() {
+    this.setData({
+      show: false
+    })
+  },
   /**
    * 生命周期函数--监听页面隐藏
    */
