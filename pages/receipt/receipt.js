8d382285779cf3f9658d520f98552f266b8b867b
lb <154430355@qq.com>
2020-07-30 15:54:45 +0800

    1

--- pages/receipt/receipt.js
+++ pages/receipt/receipt.js
@@ -1,22 +1,103 @@
 // pages/receipt/receipt.js
+var utils = require("../../utils/util.js")
+const app = getApp();
+var pageM = [];
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
+    Monthlist: [],
+    Daylist: [],
     section: [{
-      name: '待发货/受理',
-      typeId: '0'
+      name: '按月汇总',
+      typeId: '0',
+      GroupBy:7,
     }, {
-      name: '已发货/已受理',
-      typeId: '1'
-    }, {
-      name: '已完成',
-      typeId: '2'
+      name: '按日汇总',
+      typeId: '1',
+      GroupBy:10,
     }],
-    currentId:0
+    currentId:0,
+    Month:'',
+
   },
+
+
+  GetData: function() {
+    let that = this;
+    var quantity=that.data.currentId;
+    var pageindex = pageM[0].page;
+    //显示 加载中的提示
+    wx.showLoading({
+      title: '数据加载中...',
+    })
+    var data = {};
+    data.pageIndex=pageindex,
+    data.pageSize=10,
+    data.pShopID = app.globalData.AppStaffInfo.ShopID,
+    data.pGroupBy = that.data.section[quantity].GroupBy,
+    data.pMonth = that.data.Month,
+    utils.AjaxRequest(app.globalData.apiurl + "CouponView/CouponProductView/GetMonthWriteOffOrder", "POST", data, app.globalData.appkeyid, this.GetDataBack)
+  },
+  GetDataBack: function(json) {
+    let that = this;
+    var json = json.data.Data;
+    if (json.flag) {
+      if (that.data.currentId == 0) {
+        that.setData({
+          Monthlist:json.data
+        })
+      }
+
+      if (that.data.currentId == 1) {
+        if(that.data.Month!=''){
+          that.setData({
+            Daylist:json.data
+          })
+        }
+        else{
+          pageM[0].lastpage = json.pageCount;
+          if(pageM[0].page==1){
+            that.setData({
+              Daylist:json.data
+            })
+          }
+          else {
+            //获取上次加载的数据
+            var oldlists = that.data.Daylist;
+            var newlists = oldlists.concat(json.data) //合并数据 res.data 你的数组数据
+  
+            that.setData({
+              Daylist: newlists
+            });
+          }
+        }
+      }
+    } else {
+      wx.showToast({
+        title: '暂无数据!',
+        icon: "none",
+        duration: 2000
+      })
+    }
+    //隐藏 加载中的提示
+    wx.hideLoading();
+  },
+
+
+  DaySummary:function(e){
+    let that = this;
+    let time = e.currentTarget.dataset.time;
+    that.setData({
+      Month:time,
+      currentId:1
+    })
+    that.GetData();
+  },
+
+
 
   /**
    * 生命周期函数--监听页面加载
@@ -31,11 +112,11 @@ Page({
     if (id) {
       this.setData({
         currentId: id,
-      })
-
-    
+        Month:''
+      })    
     }
-
+    pageM[0].page=1;
+    that.GetData();
   },
   /**
    * 生命周期函数--监听页面初次渲染完成
@@ -48,7 +129,12 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+    pageM = [{
+      page: 1,
+      lastpage: 0
+    }];
+    let that = this;
+    that.GetData();
   },
 
   /**
@@ -76,7 +162,18 @@ Page({
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-
+    let that = this;
+    if (pageM[0].lastpage > pageM[0].page) {
+      pageM[0].page++;
+      that.GetData();
+    } else if (pageM[0].lastpage == pageM[0].page) {
+      pageM[0].page++
+        wx.showToast({
+          title: '没有更多数据!',
+          icon: 'none',
+          duration: 2000
+        })
+    }
   },
 
   /**
