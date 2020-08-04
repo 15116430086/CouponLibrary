b1fc010f96a632e92c6cb7e69262896361798faf
huawei <304171048@qq.com>
2020-08-04 13:36:31 +0800

    1

--- app.js
+++ app.js
@@ -1,21 +1,25 @@
 //app.js
 var utils = require("utils/util.js")
-
+const accountInfo = wx.getAccountInfoSync();
 App({
-    onLaunch: function() {
+    onLaunch: function () {
         // 展示本地存储能力
         var logs = wx.getStorageSync('logs') || []
         logs.unshift(Date.now())
         wx.setStorageSync('logs', logs)
+        this.globalData.sysaAppid = accountInfo.miniProgram.appId
+        wx.setNavigationBarTitle({
+            title: this.globalData.sysName + '商家助手'
+        })
 
         // 登录
         wx.login({
-                success: res => {
-                    // 发送 res.code 到后台换取 openId, sessionKey, unionId      
-                    this.globalData.logincode = res.code;
-                }
-            })
-            // 获取用户信息
+            success: res => {
+                // 发送 res.code 到后台换取 openId, sessionKey, unionId      
+                this.globalData.logincode = res.code;
+            }
+        })
+        // 获取用户信息
         wx.getSetting({
             success: res => {
                 if (res.authSetting['scope.userInfo']) {
@@ -25,8 +29,8 @@ App({
                         success: res => {
                             // 可以将 res 发送给后台解码出 unionId
                             this.globalData.userInfo = res.userInfo
-                                // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-                                // 所以此处加入 callback 以防止这种情况
+                            // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+                            // 所以此处加入 callback 以防止这种情况
                             if (this.userInfoReadyCallback) {
                                 this.userInfoReadyCallback(res)
                             }
@@ -43,19 +47,21 @@ App({
         AppStaffInfo: null,
         AppGroupInfo: {
             GroupID: 100000,
-            GroupName: "湖南券库移分公司"
+            GroupName: "湖南券库"
         },
         appkeyid: "",
         latitudeX: 28.22778,
         longitudeY: 112.93886,
         regionName: "",
+        sysName: "世纪",
+        sysaAppid: '',
 
         // apiurl: "https://wx.wap.quankuzg.com/LibraryAPI/",
         // minmapkey: 'LC5BZ-J343R-M5AWH-WYNV2-P5UW5-BVFHE',
 
-        apiurl: "https://wx.wap.meiguwen.com/LibraryAPI/",
-         minmapkey: 'UXCBZ-XKZWU-YHRVE-4RMRW-J63PJ-WRFPI',
-
+        apiurl: "https://wx.wap.quankuzg.com/PleasureAPI/",
+        minmapkey:'DV7BZ-GESWW-XKVRT-OE2FG-C2EN2-NDFYF',
+        
         //apiurl: "http://test.miboon.com:8080/LibraryAPI/",
         // minmapkey: 'UXCBZ-XKZWU-YHRVE-4RMRW-J63PJ-WRFPI',
         // apiurl: "http://localhost:7562/"
--- pages/home/home.js
+++ pages/home/home.js
@@ -12,7 +12,7 @@ Page({
     lastpage: 0,
     GroupName: "",
     noticeList: [{
-      title: "A商家领用了100张【10元代金券】"
+        title: "A商家领用了100张【10元代金券】"
       },
       {
         title: "B商家领用了100张【10元代金券】"
@@ -22,7 +22,7 @@ Page({
       },
     ]
   },
-  onWxScanCode: function() {
+  onWxScanCode: function () {
     // wx.navigateTo({
     //   url: '../scanCheck/scanCheck?CouponCode=115901'
     // }); 
@@ -35,13 +35,13 @@ Page({
       }
     })
   },
-  GetData: function() {
+  GetData: function () {
     let that = this;
     var data = {};
     data.pGroupID = app.globalData.AppGroupInfo.GroupID;
     utils.AjaxRequest(app.globalData.apiurl + "CouponView/CouponHomeView/GetHomeCouponCount", "POST", data, app.globalData.appkeyid, that.GetDataBack)
   },
-  GetDataBack: function(json) {
+  GetDataBack: function (json) {
     let that = this;
     console.log(json);
     var data = json.data.Data;
@@ -53,27 +53,27 @@ Page({
     }
   },
 
-  Jumptap: function() {
+  Jumptap: function () {
     wx.navigateTo({
       url: '../getTicketList/getTicketList?quick=0',
     })
   },
 
-  noticetap: function() {
+  noticetap: function () {
     wx.navigateTo({
       url: '../sysNotice/sysNotice',
     })
   },
 
 
-  JumpwatchMesOne: function(e) {
+  JumpwatchMesOne: function (e) {
     var couponcode = e.currentTarget.dataset.id;
     wx.navigateTo({
       url: '../sendTicketMes/sendTicketMes',
     })
   },
 
-  perfecttap: function(e) {
+  perfecttap: function (e) {
     let index = e.currentTarget.dataset.index;
     console.log(index)
     switch (index) {
@@ -113,21 +113,26 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function(options) {
-
+  onLoad: function (options) {
+    this.setData({
+      proname: app.globalData.sysName
+    })
+    wx.setNavigationBarTitle({
+      title: app.globalData.sysName + '商家助手'
+    })
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function() {
+  onReady: function () {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function() {
+  onShow: function () {
     let that = this;
     that.setData({
       GroupName: app.globalData.AppShopInfo.ShopName
@@ -138,35 +143,35 @@ Page({
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function() {
+  onHide: function () {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function() {
+  onUnload: function () {
     console.log("返回");
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function() {
+  onPullDownRefresh: function () {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function() {
+  onReachBottom: function () {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function() {
+  onShareAppMessage: function () {
 
   }
 })
