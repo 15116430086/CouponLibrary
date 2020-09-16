e9686ee1070062ba28e79c77005e60a04ecf101d
huawei <304171048@qq.com>
2020-09-16 10:14:49 +0800

    1

--- app.js
+++ app.js
@@ -53,14 +53,14 @@ App({
         latitudeX: 28.22778,
         longitudeY: 112.93886,
         regionName: "",
-        sysName: "券库",
+        sysName: "世纪",
         sysaAppid: '',
 
-        apiurl: "https://wx.wap.quankuzg.com/LibraryAPI/",
-        minmapkey: 'LC5BZ-J343R-M5AWH-WYNV2-P5UW5-BVFHE',
+        // apiurl: "https://wx.wap.quankuzg.com/LibraryAPI/",
+        // minmapkey: 'LC5BZ-J343R-M5AWH-WYNV2-P5UW5-BVFHE',
 
-        // apiurl: "https://wx.wap.quankuzg.com/PleasureAPI/",
-        // minmapkey:'DV7BZ-GESWW-XKVRT-OE2FG-C2EN2-NDFYF',
+        apiurl: "https://wx.wap.quankuzg.com/PleasureAPI/",
+        minmapkey:'DV7BZ-GESWW-XKVRT-OE2FG-C2EN2-NDFYF',
 
         // apiurl: "https://wx.wap.quankuzg.com/TestAPI/",
         // minmapkey:'DV7BZ-GESWW-XKVRT-OE2FG-C2EN2-NDFYF',
