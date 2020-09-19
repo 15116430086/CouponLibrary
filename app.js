3cfeec7aad430a24ba7dfdbde2a545d394663e91
linmxxx <348449223@qq.com>
2020-09-19 11:37:14 +0800

    111

--- app.js
+++ app.js
@@ -56,14 +56,14 @@ App({
         sysName: "券库",
         sysaAppid: '',
 
-        apiurl: "https://wx.wap.quankuzg.com/LibraryAPI/",
-        minmapkey: 'LC5BZ-J343R-M5AWH-WYNV2-P5UW5-BVFHE',
+        // apiurl: "https://wx.wap.quankuzg.com/LibraryAPI/",
+        // minmapkey: 'LC5BZ-J343R-M5AWH-WYNV2-P5UW5-BVFHE',
 
         // apiurl: "https://wx.wap.quankuzg.com/PleasureAPI/",
-        // minmapkey:'DV7BZ-GESWW-XKVRT-OE2FG-C2EN2-NDFYF',
+        minmapkey:'DV7BZ-GESWW-XKVRT-OE2FG-C2EN2-NDFYF',
 
         // apiurl: "https://wx.wap.quankuzg.com/TestAPI/",
         // minmapkey:'DV7BZ-GESWW-XKVRT-OE2FG-C2EN2-NDFYF',
-        // apiurl: "http://localhost:7562/"
+        apiurl: "http://localhost:7562/"
     }
 })
