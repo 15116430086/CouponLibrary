042b34e8f32feccca790960970d1ff42e14a57df
huawei <304171048@qq.com>
2020-11-21 16:19:44 +0800

    1

--- app.js
+++ app.js
@@ -57,10 +57,10 @@ App({
         sysaAppid: '',
 
     //    apiurl: "https://wx.wap.quankuzg.com/LibraryAPI/",
-    //     minmapkey: 'LC5BZ-J343R-M5AWH-WYNV2-P5UW5-BVFHE',
+        minmapkey: 'LC5BZ-J343R-M5AWH-WYNV2-P5UW5-BVFHE',
 
        apiurl: "https://wx.wap.quankuzg.com/PleasureAPI/",
-       minmapkey: 'DV7BZ-GESWW-XKVRT-OE2FG-C2EN2-NDFYF',
+    //    minmapkey: 'DV7BZ-GESWW-XKVRT-OE2FG-C2EN2-NDFYF',
 
         // apiurl: "https://wx.wap.quankuzg.com/TestAPI/",
         // minmapkey:'DV7BZ-GESWW-XKVRT-OE2FG-C2EN2-NDFYF',
